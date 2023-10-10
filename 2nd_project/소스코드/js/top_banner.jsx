b19cc0c4ff2ea4eebf48bb45d972547cc9cf417d
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-10 17:34:25 +0900

    cm

--- 2nd_project/소스코드/js/top_banner.jsx
+++ 2nd_project/소스코드/js/top_banner.jsx
@@ -1,7 +1,9 @@
 function TopBanner() {
     return (
         <React.Fragment>
-            
+            <a href="#">카카오톡 채널 추가 시 3천원 추가 할인!</a>
         </React.Fragment>
     );
 }
+
+ReactDOM.render(<TopBanner />, document.querySelector(".top-banner"));
