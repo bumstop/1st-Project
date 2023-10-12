d49a9604d38e14ee2e60cb4d408640f4a1ac92d7
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-12 18:19:30 +0900

    cm

--- 2nd_project/소스코드/js/header.jsx
+++ 2nd_project/소스코드/js/header.jsx
@@ -1,25 +1,65 @@
 function Header() {
     return (
         <div class="gnb">
-            <div class="hamburger">
+            <div class="hamburger m-r-2p">
                 <span class="petty"></span>
             </div>
             <div class="top-logo">
                 <a href="#">
-                    <img src="./images/header_logo_bk.png" alt="xexymix" />
+                    <img
+                        src="./images/header_logo_bk.png"
+                        alt="xexymix"
+                    />
                 </a>
             </div>
             <ul class="gnb-category">
-                <li><a href="#">신상할인</a></li>
-                <li><a href="#">베스트</a></li>
-                <li><a href="#">우먼즈</a></li>
-                <li><a href="#">맨즈</a></li>
-                <li><a href="#">골프</a></li>
-                <li><a href="#">시즌오프</a></li>
-                <li><a href="#">커뮤니티</a></li>
+                <li>
+                    <a href="#">신상할인</a>
+                </li>
+                <li>
+                    <a href="#">베스트</a>
+                </li>
+                <li>
+                    <a href="#">우먼즈</a>
+                </li>
+                <li>
+                    <a href="#">맨즈</a>
+                </li>
+                <li>
+                    <a href="#">골프</a>
+                </li>
+                <li>
+                    <a href="#">시즌오프</a>
+                </li>
+                <li>
+                    <a href="#">커뮤니티</a>
+                </li>
+            </ul>
+            <ul class="right-btn-wrap">
+                <li>
+                    <a href="#">
+                        <img
+                            src="./images/menu_view.png"
+                            alt="menu_view"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img
+                            src="./images/menu_cart.png"
+                            alt="menu_cart"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="./images/menu_mypage.png" alt="menu_mypage" />
+                    </a>
+                </li>
             </ul>
         </div>
     );
 }
 
-ReactDOM.render(<Header />, document.querySelector('#header'));
+ReactDOM.render(<Header />, document.querySelector("#header"));
