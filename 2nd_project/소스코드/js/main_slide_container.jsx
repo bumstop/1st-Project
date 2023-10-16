6ef8902f2a197d0ab019ccfb5095ab1a4271a632
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-16 14:46:03 +0900

    cm

--- 2nd_project/소스코드/js/main_slide_container.jsx
+++ 2nd_project/소스코드/js/main_slide_container.jsx
@@ -1,7 +1,24 @@
 function MainSlideContainer() {
     return (
-        <div class="slide-wrapper">
-            
-        </div>
-    )
+        <React.Fragment>
+            <div class="slide-wrapper">
+                <div class="slide-item">
+                    <a href="#">
+                        <img src="./images/productimg.gif" alt="xexymix" />
+                        <div class="item-txt-box">
+                            <div class="category">[category]</div>
+                            <div class="title">[title]</div>
+                            <div class="desc">[desc]</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="slide-btn-prev"></div>
+            <div class="slide-btn-next"></div>
+            <div class="slide-pagination"></div>
+            <div class="slide-btn-pause"></div>
+        </React.Fragment>
+    );
 }
+
+ReactDOM.render(<MainSlideContainer />, document.querySelector(".main-slide-container"));
