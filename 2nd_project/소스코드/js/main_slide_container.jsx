4f670c87abc259ebc82bc22c8e262cbcbbe4309f
Tom B. Parker <tombap8@hotmail.com>
2023-10-18 18:08:14 +0900

    Update main_slide_container.jsx

--- 2nd_project/소스코드/js/main_slide_container.jsx
+++ 2nd_project/소스코드/js/main_slide_container.jsx
@@ -1,30 +1,36 @@
 import slideInfo from "./main_slide_info.jsx";
 
-// function SlideItem() {
-//     Object.keys(slideInfo).map((v) => {
-//         console.log(slideInfo[v].index);
-//             <div class="slide-item" data-index={slideInfo[v].index}>
-//                 <a href="#">
-//                     <div class="item-img-box">
-//                         <img src={slideInfo[v].imgSrc} alt="xexymix" />
-//                     </div>
-//                     <div class="item-txt-box">
-//                         <div class="category">{slideInfo[v].category}</div>
-//                         <div class="title">
-//                             <p>
-//                                 {slideInfo[v].title[0]}
-//                                 <br />
-//                                 {slideInfo[v].title[1]}
-//                             </p>
-//                         </div>
-//                         <div class="desc">{slideInfo[v].desc}</div>
-//                     </div>
-//                 </a>
-//             </div>
-//     });
-// }
+const newData = Object.values(slideInfo);
+const newData2 = Object.keys(slideInfo).map(v=>slideInfo[v]);
+console.log(slideInfo);
+console.log(newData);
+console.log(newData2.map((v) => <div>{v}</div>));
+console.log(newData.map((v) => <div>{v}</div>));
 
-// Object.keys(slideInfo).map((v) => {
+function SlideItem() {
+  return newData.map((v) => <div>{v}</div>);
+  // <div class="slide-item" data-index={props.index}>
+  //     <a href="#">
+  //         <div class="item-img-box">
+  //             <img src={props.imgSrc} alt="xexymix" />
+  //         </div>
+  //         <div class="item-txt-box">
+  //             <div class="category">{props.category}</div>
+  //             <div class="title">
+  //                 <p>
+  //                     {props.title[0]}
+  //                     <br />
+  //                     {props.title[1]}
+  //                 </p>
+  //             </div>
+  //             <div class="desc">{props.desc}</div>
+  //         </div>
+  //     </a>
+  // </div>
+  //     );
+}
+
+// Object.keys(slideInfo).map((v) =>
 //         <SlideItem
 //             index={slideInfo[v].index}
 //             imgSrc={slideInfo[v].imgSrc}
@@ -33,18 +39,53 @@ import slideInfo from "./main_slide_info.jsx";
 //             title2={slideInfo[v].title[1]}
 //             desc={slideInfo[v].desc}
 //         />
-//         console.log(slideInfo[v]);
-// });
+//         // console.log(slideInfo[v]);
+// );
 
+function MyFn(){
+    return (
+        newData2.map((v) => (
+        <div class="slide-item" data-index={v.index}>
+          <a href="#">
+            <div class="item-img-box">
+              <img src={v.imgSrc} alt="xexymix" />
+            </div>
+            <div class="item-txt-box">
+              <div class="category">{v.category}</div>
+              <div class="title">
+                <p>
+                  {v.title[0]}
+                  <br />
+                  {v.title[1]}
+                </p>
+              </div>
+              <div class="desc">{v.desc}</div>
+            </div>
+          </a>
+        </div>
+      ))
+    )
+}
 
 let slideInfoKeys = Object.keys(slideInfo);
-console.log(slideInfo[slideInfoKeys[0]].index)
+console.log(slideInfo[slideInfoKeys[0]].index);
 function MainSlideContainer() {
-    return (
-        <React.Fragment>
-            <div class="slide-wrapper">
-                {/* <SlideItem /> */}
-                <div class="slide-item" data-index={slideInfo[slideInfoKeys[0]].index}>
+  return (
+    <React.Fragment>
+        <div class="slide-wrapper">
+      <MyFn />
+      </div>
+      {/* <div class="slide-wrapper">
+                <SlideItem 
+                    index={slideInfo[v].index}
+                    imgSrc={slideInfo[v].imgSrc}
+                    category={slideInfo[v].category}
+                    title1={slideInfo[v].title[0]}
+                    title2={slideInfo[v].title[1]}
+                    desc={slideInfo[v].desc}
+                />
+            </div> */}
+      {/* <div class="slide-item" data-index={slideInfo[slideInfoKeys[0]].index}>
                     <a href="#">
                         <div class="item-img-box">
                             <img src={slideInfo[slideInfoKeys[0]].imgSrc} alt="xexymix" />
@@ -98,22 +139,25 @@ function MainSlideContainer() {
                         </div>
                     </a>
                 </div>
-            </div>
-            
-            <div class="slide-btn-prev">
-                <img src="./images/main_btn_prev.png" alt="btn_prev" />
-            </div>
-            <div class="slide-btn-next">
-                <img src="./images/main_btn_next.png" alt="btn_prev" />
-            </div>
-            <div class="slide-pagination">
-                <span class="slide-pagination-focus">1</span>
-                <span> / </span>
-                <span class="slide-pagination-total">8</span>
-            </div>
-            <div class="slide-btn-pause"></div>
-        </React.Fragment>
-    );
+            </div> */}
+
+      <div class="slide-btn-prev">
+        <img src="./images/main_btn_prev.png" alt="btn_prev" />
+      </div>
+      <div class="slide-btn-next">
+        <img src="./images/main_btn_next.png" alt="btn_prev" />
+      </div>
+      <div class="slide-pagination">
+        <span class="slide-pagination-focus">1</span>
+        <span> / </span>
+        <span class="slide-pagination-total">8</span>
+      </div>
+      <div class="slide-btn-pause"></div>
+    </React.Fragment>
+  );
 }
 
-ReactDOM.render(<MainSlideContainer />, document.querySelector(".main-slide-container"));
+ReactDOM.render(
+  <MainSlideContainer />,
+  document.querySelector(".main-slide-container")
+);
