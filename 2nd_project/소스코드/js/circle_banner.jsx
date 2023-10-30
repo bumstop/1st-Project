13289ff34d5358d628eee1875ed5fb963ac8e162
bumstop <127211764+bumstop@users.noreply.github.com>
2023-10-30 16:38:11 +0900

    cm

--- 2nd_project/소스코드/js/circle_banner.jsx
+++ 2nd_project/소스코드/js/circle_banner.jsx
@@ -1,16 +1,16 @@
 const circleBannerItems = [
-    ["yoga","요가 컬렉션"], 
-    ["cacaopay",'4천원 즉시할인'], 
-    ["tosspay", "10%할인 즉시적용"], 
-    ["present", "선물하기 서비스 오픈"], 
-    ["notice", "회원 등급별 혜택안내"]
+    ["yoga", ["요가 컬렉션", ""]],
+    ["cacaopay", ["4천원", "즉시할인"]],
+    ["tosspay", ["10%할인", "즉시적용"]],
+    ["present", ["선물하기", "서비스 오픈"]],
+    ["notice", ["회원 등급별", "혜택안내"]],
 ];
 
 function CircleBanner() {
     return circleBannerItems.map((v) => (
         <div class="circle-item">
             <img src={"./images/circle_banner/slide_" + v[0] + ".png"} alt="" />
-            <span class="circle-item-txt">{v[1]}</span>
+            <span class="circle-item-txt">{v[1][0]}<br/>{v[1][1]}</span>
         </div>
     ));
 }
