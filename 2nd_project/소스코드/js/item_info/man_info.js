0c2d211ca2739ac262e2c40898f50bec07eb175d
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-03 15:39:59 +0900

    Update man_info.js

--- 2nd_project/소스코드/js/item_info/man_info.js
+++ 2nd_project/소스코드/js/item_info/man_info.js
@@ -2,7 +2,7 @@ export default {
     item0: {
         imgSrc: ["./images/man/man1.jpg", null],
         name: "오버핏 리버서블 퍼 자켓 블랙",
-        type: ["outer"],
+        type: "outer",
         price: "159000",
         sale: "99000",
         iconContent: ["이벤트특가", "신상"],
@@ -12,7 +12,7 @@ export default {
     item2: {
         imgSrc: ["./images/man/man2.jpg", null],
         name: "커뮤터 스트레치 라이트 패딩 자켓 애쉬카키",
-        type: ["outer"],
+        type: "outer",
         price: "189000",
         sale: "119000",
         iconContent: ["이벤트특가", "~3XL", "신상"],
@@ -22,7 +22,7 @@ export default {
     item3: {
         imgSrc: ["./images/man/man3.jpg", null],
         name: "맨즈 리버서블 포켓 퀼티드 자켓 사파리카키",
-        type: ["outer"],
+        type: "outer",
         price: "139000",
         sale: "89000",
         iconContent: ["이벤트특가", "신상"],
@@ -32,7 +32,7 @@ export default {
     item4: {
         imgSrc: ["./images/man/man4.jpg", null],
         name: "텍스처드 플리스 집업 자켓 블랙",
-        type: ["outer"],
+        type: "outer",
         price: "99000",
         sale: "69000",
         iconContent: ["~2XL", "신상"],
@@ -42,7 +42,7 @@ export default {
     item5: {
         imgSrc: ["./images/man/man5.jpg", null],
         name: "커뮤터 스트레치 라이트 롱 패딩 블랙",
-        type: ["outer"],
+        type: "outer",
         price: "199000",
         sale: "169000",
         iconContent: ["~3XL", "신상"],
@@ -52,7 +52,7 @@ export default {
     item6: {
         imgSrc: ["./images/man/man6.gif", null],
         name: "리버서블 퀼팅 플리스 코트 블랙",
-        type: ["outer"],
+        type: "outer",
         price: "169000",
         sale: "119000",
         iconContent: ["이벤트특가", "~2XL", "신상"],
@@ -62,7 +62,7 @@ export default {
     item7: {
         imgSrc: ["./images/man/man7.jpg", null],
         name: "넥리스 심플 경량 패딩 다크그레이",
-        type: ["outer"],
+        type: "outer",
         price: "119000",
         sale: "99000",
         iconContent: ["~2XL", "신상"],
@@ -72,7 +72,7 @@ export default {
     item8: {
         imgSrc: ["./images/man/man8.jpg", null],
         name: "플렉스 집업 후디",
-        type: ["outer"],
+        type: "outer",
         price: "72000",
         sale: "49000",
         iconContent: ["~2XL", "신상"],
@@ -82,7 +82,7 @@ export default {
     item9: {
         imgSrc: ["./images/man/man9.jpg", null],
         name: "유틸라이트 포텐셜 롱슬리브 블랙",
-        type: ["top"],
+        type: "top",
         price: "49000",
         sale: "15000",
         iconContent: ["이벤트특가", "신상"],
@@ -92,7 +92,7 @@ export default {
     item10: {
         imgSrc: ["./images/man/man10.jpg", null],
         name: "프렌치 테리 하프집업 스웨트셔츠 네이비",
-        type: ["top"],
+        type: "top",
         price: "82000",
         sale: "59000",
         iconContent: ["~2XL", "신상"],
@@ -102,7 +102,7 @@ export default {
     item11: {
         imgSrc: ["./images/man/man11.jpg", null],
         name: "스포츠 윈드 테크 롱슬리브 블랙",
-        type: ["top"],
+        type: "top",
         price: "82000",
         sale: "59000",
         iconContent: ["~3XL", "신상"],
@@ -112,7 +112,7 @@ export default {
     item12: {
         imgSrc: ["./images/man/man12.jpg", null],
         name: "필드 하프집업 롱슬리브 1+1",
-        type: ["top"],
+        type: "top",
         price: "98000",
         sale: "50000",
         iconContent: ["이벤트특가", "~2XL", "신상"],
@@ -122,7 +122,7 @@ export default {
     item13: {
         imgSrc: ["./images/man/man13.jpg", null],
         name: "트리플 엑스 숏슬리브 1+1",
-        type: ["top"],
+        type: "top",
         price: "58000",
         sale: "44800",
         iconContent: ["~3XL", "신상"],
@@ -132,7 +132,7 @@ export default {
     item14: {
         imgSrc: ["./images/man/man14.jpg", null],
         name: "하이쿨링 테크 숏슬리브 1+1",
-        type: ["top"],
+        type: "top",
         price: "88000",
         sale: "49800",
         iconContent: ["~3XL", "신상"],
@@ -142,7 +142,7 @@ export default {
     item15: {
         imgSrc: ["./images/man/man15.jpg", null],
         name: "베이직 로고 트랙 SET",
-        type: ["top", "bottom"],
+        type: "set",
         price: "133000",
         sale: "89800",
         iconContent: ["~2XL", "신상"],
@@ -152,7 +152,7 @@ export default {
     item16: {
         imgSrc: ["./images/man/man16.jpg", null],
         name: "우븐 스트레치 버닝 SET",
-        type: ["top", "bottom"],
+        type: "set",
         price: "163000",
         sale: "109800",
         iconContent: ["~2XL", "신상"],
