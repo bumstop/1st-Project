5c9d7d0d0e379015ca063d03277cdbe1bc28e5d0
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-13 17:59:07 +0900

    cm

--- 2nd_project/xexymix_react/src/components/main_item_container.jsx
+++ 2nd_project/xexymix_react/src/components/main_item_container.jsx
@@ -1,7 +1,10 @@
 import { manItemInfo } from "../data/man_item_info";
+import { womanItemInfo } from "../data/woman_item_info";
+
 import { ItemBox } from "./item_box.jsx"; // props 로 import받은 Object의 value를 넘겨준다
 
 const manItemInfoValues = Object.values(manItemInfo);
+const womanItemInfoValues = Object.values(womanItemInfo);
 // props 로 import받은 Object의 value를 넘겨준다
 // <ItemBox info={manInfoValues}/>
 
@@ -13,7 +16,9 @@ export function MainItemContainer() {
         <div className="item-box-wrap man-item-box-wrap">
           <ItemBox info={manItemInfoValues} />
         </div>
-        <div className="top-item"></div>
+        <div className="item-box-wrap woman-item-box-wrap">
+          <ItemBox info={womanItemInfoValues} />
+        </div>
         <div className="bottom-item"></div>
       </div>
     </div>
