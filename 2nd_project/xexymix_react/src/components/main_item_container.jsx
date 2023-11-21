69781230c944294679a4219f0b9eceb43fb0bff6
bumstop <127211764+bumstop@users.noreply.github.com>
2023-11-21 15:40:28 +0900

    cm

--- 2nd_project/xexymix_react/src/components/main_item_container.jsx
+++ 2nd_project/xexymix_react/src/components/main_item_container.jsx
@@ -7,17 +7,61 @@ const manItemInfoValues = Object.values(manItemInfo);
 const womanItemInfoValues = Object.values(womanItemInfo);
 
 export function MainItemContainer() {
-  let filterByType = ["outer", "bottom"];
+  /**
+   * val값을 모두 가지고 있는 객체를 리턴
+   * @param {object} obj 필터 대상 객체
+   * @param {string} key 필터링 기준이 되는 key 문자열
+   * @param {Array} val 필터링 기준이 되는 value를 담은 배열
+   * @returns obj를 리턴, key값의 val로 필터링
+   */
+  const filteredItemEvery = (obj, key, val) =>
+    obj.filter((v) => val.every((i) => v[key].includes(i)));
 
-  const filteredItemArr = (filterBy, filterBy) =>
-    manItemInfoValues.filter((v) => filterBy.every((i) => v.filterBy.includes(i)));
+  /**
+   * val값을 하나라도 가지고 있는 객체를 리턴
+   * @param {object} obj 필터 대상 객체
+   * @param {string} key 필터링 기준이 되는 key 문자열
+   * @param {Array} val 필터링 기준이 되는 value를 담은 배열
+   * @returns obj를 리턴, key값의 val로 필터링
+   */
+  const filteredItemSome = (obj, key, val) =>
+    obj.filter((v) => v[key].some((i) => val.includes(i)));
 
-  console.log(filteredItemArr(filterByType, ));
+  /**
+   * val값을 가지고 있는 객체를 리턴
+   * @param {object} obj 필터 대상 객체
+   * @param {string} key 필터링 기준이 되는 key 문자열
+   * @param {string} val 필터링 기준이 되는 value를 담은 문자열
+   * @returns obj를 리턴, key값의 val로 필터링
+   */
+  const filteredItem = (obj, key, val) =>
+    obj.filter((v) => v[key].includes(val));
+
+  /**
+   * val값 하나만 가지고 있는 객체를 리턴
+   * @param {object} obj 필터 대상 객체
+   * @param {string} key 필터링 기준이 되는 key 문자열
+   * @param {string} val 필터링 기준이 되는 value를 담은 문자열
+   * @returns obj를 리턴, key값의 val로 필터링
+   */
+  const filteredItemOne = (obj, key, val) =>
+    obj.filter((v) => v[key].length == 1 && v[key].includes(val));
+
+  // console.log(filteredItemOnce(manItemInfoValues, "type", "top"));
 
   return (
     <div className="main-item-container">
       <div className="seeing-box"></div>
       <div className="main-item-box">
+        {/* 여성 아우터 */}
+        <div className="item-box-wrap outer-item-box-wrap">
+          {filteredItem(womanItemInfoValues, "type", "outer").map((v, i) => (
+            <div className="item-box" data-index={i} key={v.name}>
+              {makeItemBox(v)}
+            </div>
+          ))}
+        </div>
+        {/* 남성 */}
         <div className="item-box-wrap man-item-box-wrap">
           {manItemInfoValues.map((v, i) => (
             <div className="item-box" data-index={i} key={v.name}>
@@ -25,13 +69,23 @@ export function MainItemContainer() {
             </div>
           ))}
         </div>
-        <div className="item-box-wrap woman-item-box-wrap">
-          {womanItemInfoValues.map((v, i) => (
+        {/* 여성 상의 */}
+        <div className="item-box-wrap woman-top-item-box-wrap">
+          {filteredItemOne(womanItemInfoValues, "type", "top").map((v, i) => (
             <div className="item-box" data-index={i} key={v.name}>
               {makeItemBox(v)}
             </div>
           ))}
         </div>
+        {/* 여성 하의 */}
+        <div className="item-box-wrap woman-bottom-item-box-wrap">
+          {filteredItemOne(womanItemInfoValues, "type", "bottom").map((v, i) => (
+            <div className="item-box" data-index={i} key={v.name}>
+              {makeItemBox(v)}
+            </div>
+          ))}
+        </div>
+
         <div className="bottom-item"></div>
       </div>
     </div>
