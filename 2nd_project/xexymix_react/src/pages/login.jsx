48aebe8c40f65bf88365fe47150fb889d5a4252e
bumstop <bums1290@gmail.com>
2024-01-04 18:20:27 +0900

    cm

--- 2nd_project/xexymix_react/src/pages/login.jsx
+++ 2nd_project/xexymix_react/src/pages/login.jsx
@@ -2,9 +2,12 @@ import { faComment } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import React from "react";
-import { Link } from "react-router-dom";
 
 export function Login() {
+  const makeMovePlaceholder = (e) => {
+    const text = e.target.placeholder;
+  };
+
   return (
     <div className="login-container">
       <div className="kakao-login-box">
@@ -17,6 +20,39 @@ export function Login() {
           <FontAwesomeIcon icon={faComment} />
           카카오 1초 로그인/회원가입
         </a>
+        <div className="kakao_benefit">
+          <img
+            src={`${process.env.PUBLIC_URL}/images/kakao_benefit.jpg`}
+            alt="카카오 혜택"
+          />
+        </div>
+      </div>
+      <div className="divide-line">또는</div>
+      <div className="xexymix-login-box">
+        <div className="id-box-wrap">
+          <input
+            type="text"
+            id="id-box"
+            name="id"
+            placeholder="아이디"
+            maxLength="20"
+            onFocus={makeMovePlaceholder}
+          />
+        </div>
+        <div className="pwd-box-wrap">
+          <input
+            type="password"
+            id="pwd-box"
+            name="password"
+            placeholder="비밀번호"
+            maxLength="20"
+            onFocus={makeMovePlaceholder}
+          />
+        </div>
+        <div className="memorize-id-box">
+          <input type="checkbox" name="memorize-id" id="memorize-id" />
+          <label for="memorize-id">아이디 저장</label>
+        </div>
       </div>
     </div>
   );
